9d2b4bfe2772341d093e4a5bd95d8e64e8c4b65e
sonworks <csw0413@outlook.com>
2023-03-26 13:15:45 +0900

    Add custom actionBar

--- app/src/main/java/com/example/listuserssample/MainActivity.kt
+++ app/src/main/java/com/example/listuserssample/MainActivity.kt
@@ -2,15 +2,32 @@ package com.example.listuserssample
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.listuserssample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+        val navController = findNavController(R.id.nav_host)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host)
+        return navController.navigateUp() ||
+                super.onSupportNavigateUp()
     }
 }
