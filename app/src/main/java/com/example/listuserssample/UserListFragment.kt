8b017ebcea2e2f4497b7095a56223c726b6f945e
sonworks <csw0413@outlook.com>
2023-03-26 12:34:19 +0900

    Add navigation

---
+++ app/src/main/java/com/example/listuserssample/UserListFragment.kt
@@ -0,0 +1,38 @@
+package com.example.listuserssample
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.listuserssample.databinding.FragmentUserListBinding
+
+class UserListFragment : Fragment() {
+
+    private var _binding: FragmentUserListBinding? = null
+    private val binding
+        get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentUserListBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.nextButton.setOnClickListener {
+            findNavController().navigate(R.id.action_UserListFragment_to_UserDetailFragment)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
